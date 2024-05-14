3735c5c02625ea7a584f6283210f80307823fbf0
ches <cheskacmojica@gmail.com>
2024-05-14 15:00:01 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import time
+import os
+import sys
 
-# Main function to run the Streamlit application
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
+
+import Results  
+
 def main():
     icon = "./RecycleMateLogo.ico"
 
@@ -10,52 +16,57 @@ def main():
         page_icon=icon,
     )
 
-    with st.sidebar:
-        col1, mid, col2 = st.columns([4,1,20])
-        
-        with col1:            
-            st.image(icon,width=60)
-        with col2:
-            st.title("RecycleMate")
-    
-    st.title("Welcome to RecycleMate!")
+    if 'page' not in st.session_state:
+        st.session_state.page = 'home'
 
-    st.markdown(
-        """
-        RecycleMate is a web application designed to assist you in practicing recycling and contributing to environmental sustainability.
+    if st.session_state.page == 'home':
+        with st.sidebar:
+            col1, mid, col2 = st.columns([4, 1, 20])
+            with col1:
+                st.image(icon, width=60)
+            with col2:
+                st.title("RecycleMate")
 
-        ### Instructions
-        1. **Upload or Capture an Image**: Upload an image of the recycling material from your device or capture an image using 
-        your device's camera.
-        2.  **Generate Recommendations**: After uploading or capturing the image, click on the "Generate" button to initiate 
-        the analysis process.
-        3. **Explore Recycling Projects**: Once the analysis is complete, RecycleMate will display a list of recommended recycling 
-        projects based on the identified materials in the image. Explore the list to find initiatives that interest you and 
-        contribute to a greener planet!
-    """
-    )
+        st.title("Welcome to RecycleMate!")
 
-    # Create tabs for uploading image and capturing image
-    tab1, tab2, = st.tabs(["Upload Image", "Capture Image"])
+        st.markdown(
+            """
+            RecycleMate is a web application designed to assist you in practicing recycling and contributing to environmental sustainability.
 
-    with tab1:
-        # Upload image option
-        upload_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
-        if upload_image:
-            st.image(upload_image, caption="Uploaded Image", use_column_width=True)
-    with tab2:
-        # Capture image option
-        capture_image = st.camera_input("Capture Image")
+            ### Instructions
+            1. **Upload or Capture an Image**: Upload an image of the recycling material from your device or capture an image using 
+            your device's camera.
+            2. **Generate Recommendations**: After uploading or capturing the image, click on the "Generate" button to initiate 
+            the analysis process.
+            3. **Explore Recycling Projects**: Once the analysis is complete, RecycleMate will display a list of recommended recycling 
+            projects based on the identified materials in the image. Explore the list to find initiatives that interest you and 
+            contribute to a greener planet!
+            """
+        )
 
-    # Generate recommendations button
-    if upload_image or capture_image:
-        st.write("The item in the image is a: *insert item*")
-        if st.button("Generate Recommendations"):
-            with st.spinner('Generating Recommendations...'):
-                time.sleep(3)
-                st.text("*insert recommendations*")
-    
+        tab1, tab2 = st.tabs(["Upload Image", "Capture Image"])
 
-        
+        with tab1:
+            upload_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+            if upload_image:
+                st.image(upload_image, caption="Uploaded Image", use_column_width=True)
+                st.session_state.uploaded_image = upload_image
+        with tab2:
+            capture_image = st.camera_input("Capture Image")
+            if capture_image:
+                st.session_state.captured_image = capture_image
+
+        if upload_image or capture_image:
+            st.write("The item in the image is a: *insert item*")
+            if st.button("Generate Recommendations"):
+                with st.spinner('Generating Recommendations...'):
+                    time.sleep(3)
+                    st.session_state.recommendations = ["Cardboard", "Paper Cup", "Plastic Spoon"] #PLACEHOLDER
+                    st.session_state.page = 'results'
+                    st.experimental_rerun()
+
+    elif st.session_state.page == 'results':
+        Results.main()
+
 if __name__ == "__main__":
     main()

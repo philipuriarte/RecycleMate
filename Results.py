de67b191077ba852ce434550a56ea64a10269e4c
Lalaine Joy Bejarin <lalaine.bejarin@yahoo.com>
2024-05-16 18:46:54 +0800

    add websites for other materials

--- Results.py
+++ Results.py
@@ -22,10 +22,30 @@ websites = [
     "https://www.diybunker.com/68-diy-recycle-project-ideas-thatre-totally-genius/",
     "https://petticoatjunktion.com/crafts/upcycled-aluminum-can-decor/",
     "https://www.familyholiday.net/55-creative-bottle-cap-craft-ideas-diy-recycle-projects/#google_vignette",
+    "https://www.diytomake.com/37-diy-ways-to-recycle-bottle-caps/#:~:text=20%20Smart%20Ways%20To%20Recycle%20Bottle%20Caps%20And,8.%20DIY%20Bottle%20Cap%20Basket%20...%20More%20items",
+    "https://www.diyncrafts.com/17424/repurpose/50-jaw-dropping-ideas-for-upcycling-tin-cans-into-beautiful-household-items",
+    "https://singlegirlsdiy.com/candy-wrapper-crafts/",
     "https://www.hellowonderful.co/post/10-creative-ways-to-recycle-cardboard-into-kids-crafts/",
     "https://www.diyncrafts.com/27010/repurpose/35-brilliant-diy-repurposing-ideas-cardboard-boxes",
-    "https://www.diyncrafts.com/110523/decor/old-book-crafts-and-decorations"
+    "https://veryinformed.com/can-you-recycle-chip-bags/#:~:text=How%20Can%20I%20Reuse%20and%20Repurpose%20My%20Old,Baby%20Toys%20...%205%205.%20Party%20Decorations%20",
+    "https://www.diyncrafts.com/16784/repurpose/35-impossibly-creative-projects-you-can-make-with-recycled-egg-cartons",
+    "https://organisemyhouse.com/egg-carton-reuse/",
+    "https://organisemyhouse.com/egg-carton-reuse/",
+    "https://www.upcycleart.info/crafts/glass-jars-recycled-decor-crafts/",
+    "https://wonderfuldiy.com/recycled-mason-jar-lids/",
+    "https://craftinvaders.co.uk/recycled-magazine-basket/",
+    "https://www.diyncrafts.com/110523/decor/old-book-crafts-and-decorations",
+    "https://energytheory.com/how-to-recycle-plastic-at-home/#:~:text=How%20to%20Recycle%20Plastic%20Properly%20at%20Home%201,Garden%20Using%20Old%20Plastic%20Pipes%20...%20More%20items",
+    "https://digginginthegarden.com/diy-recycled-plastic-bottle-gardens/",
+    "https://www.ecomasteryproject.com/reuse-plastic-cups/#:~:text=40%20Brilliant%20Ways%20to%20Reuse%20Plastic%20Cups%20for,8%208.%20Greenhouse%20for%20Seedlings%20...%20More%20items",
+    "https://www.onegoodthingbyjillee.com/40-clever-ways-to-reuse-plastic-lids/#:~:text=1%20Place%20plastic%20lids%20under%20the%20legs%20of,to%20draw%20fun%20shapes%20onto%20plastic%20lids.%20",
+    "https://www.familyhandyman.com/list/21-nifty-ways-to-reuse-plastic-jugs-and-bottles-at-home/",
+    "https://www.boredart.com/2019/08/repurposing-plastic-straw-crafts-ideas.html",
+    "https://www.ecomasteryproject.com/shampoo-bottles-reuse-ideas/#:~:text=Easy%20Shampoo%20Bottles%20Reuse%20Ideas%20For%20Minimum%20Waste,hanging%20garden%20...%208%20Sprinkler%20...%20More%20items",
+    "https://get-green-now.com/reuse-styrofoam/",
+    "https://simplelifeofalady.com/recycle-tires/#:~:text=20%20Amazing%20Ideas%20to%20Recycle%20Tires%201%201.,...%208%208.%20Toy%20storage%20...%20More%20items"
 ]
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
